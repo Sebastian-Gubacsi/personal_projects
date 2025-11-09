176baa230cadb217c4e84cf1886c10f5f72bd745
Sebastian-Gubacsi <sebastian.gubacsi@gmail.com>
2025-11-09 13:09:20 -0500

    -

--- python_database/main.py
+++ python_database/main.py
@@ -10,6 +10,16 @@ connection.execute('''
                     );
 ''')
 
+# insert data into the table
+BooksData = [
+    ('The First Programmer', 'Andy Hunt', 1999),
+    ('Head First Python', 'Paul Barry', 2010),
+    ('Clean Code', 'Robert C. Martin', 2008),
+    ('The Pragmatic Programmer', 'Andrew Hunt and David Thomas', 1999),
+    ('Introduction to Algorithms', 'Thomas H. Cormen', 2009)
+]
+connection.execute('INSERT INTO books(title, author, year) VALUES (?, ?, ?);', BooksData[0])
+
 # writes data to database
 connection.commit()
 
